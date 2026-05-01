bc54744fe9f13a33cc5c70334146ad453f497f7f
nomad4tech <alex.sav4387@gmail.com>
2026-05-01 20:39:27 +0300

    Add JsonInclude annotation to InlineKeyboardButton
    - Include non-null properties in JSON serialization

--- src/main/java/tech/nomad4/telegrambot4j/model/InlineKeyboardButton.java
+++ src/main/java/tech/nomad4/telegrambot4j/model/InlineKeyboardButton.java
@@ -1,6 +1,7 @@
 package tech.nomad4.telegrambot4j.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import lombok.Data;
  */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class InlineKeyboardButton {
 
     @JsonProperty("text")
